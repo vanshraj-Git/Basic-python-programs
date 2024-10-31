1f0d99228bd7cc44e6bb3965ccbc286a1098a1a7
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-10-31 08:00:35 +0530

    Add files via upload

---
+++ Star-pattern-programs./diamond.py
@@ -0,0 +1,14 @@
+n=5
+for i in range (0,n):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(0,i+1):
+        print("* ",end="")
+    print() 
+for i in range (1,n):
+    for j in range (0,i+1):
+        print(" ",end="")   
+    for j in range(i,n):
+        print("* ",end="")
+    print()
+   
